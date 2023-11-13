0881c0402a87a2bef184491c378586d923c3374a
jaanvi <junejajaanvi@gmail.com>
2023-11-13 14:19:55 -0500

    new queries

--- join-queries.sql
+++ join-queries.sql
@@ -1,4 +1,4 @@
-# Display authors who haven't written any books.
+-- Display authors who haven't written any books.
 
 select a.author_name
 from books as b
@@ -6,7 +6,7 @@ right join authors as a
 on b.author = a.author_name
 where b.title IS NULL;
 
-# Determine the average price of books for each nationality.
+-- Determine the average price of books for each nationality.
 
 select a.nationality, avg(b.price)
 from books as b
@@ -14,7 +14,7 @@ join authors as a
 on b.author = a.author_name
 group by a.nationality;
 
-# Find all books along with their author's name and nationality.
+-- Find all books along with their author's name and nationality.
 
 select distinct b.title, a.author_name, a.nationality 
 from books as b
@@ -22,7 +22,7 @@ inner join
 authors as a
 on b.author = a.author_name;
 
-# Determine how many books each author has written and include the author's name in the result.
+-- Determine how many books each author has written and include the author's name in the result.
 
 select b.author, count(b.title)
 from books as b
@@ -31,7 +31,7 @@ authors as a
 on b.author = a.author_name
 group by b.author;
 
-# List all authors, their book titles, and genres, even if they have not written any book.
+-- List all authors, their book titles, and genres, even if they have not written any book.
 
 select a.author_name, b.title, b.genre
 from authors as a
@@ -39,7 +39,7 @@ full join
 books as b
 on a.author_name = b.author;
 
-# For each genre, find the author who has written the most books and list the titles of those books.
+-- For each genre, find the author who has written the most books and list the titles of those books.
 
 select a.author, a.genre
 FROM (select tablec.author, tablec.genre, tablec.noofbooks
@@ -54,7 +54,7 @@ group by genre, author) tablea
 group by genre) tableb
 on tableb.genre = tablec.genre and tableb.maxbooks = tablec.noofbooks) a;
 
-# Find the nationality that represents the highest average book price, including at least 2 authors from that nationality in your result set.
+-- Find the nationality that represents the highest average book price, including at least 2 authors from that nationality in your result set.
 
 select tablea.nationality, count(tablea.author), max(tablea.avgprice) as maxt
 from
@@ -67,7 +67,7 @@ group by a.nationality, b.author) tablea
 group by tablea.nationality
 having count(tablea.author) > 1;
 
-# Using a LEFT JOIN, identify authors who have no books in the books table and list their nationalities.
+-- Using a LEFT JOIN, identify authors who have no books in the books table and list their nationalities.
 
 select a.author_name, a.nationality
 from authors as a
@@ -75,14 +75,14 @@ left join books as b
 on a.author_name = b.author
 where b.title is Null;
 
-# Show the average book price and total number of books for each author, but only for authors who have written books costing more than $15
+-- Show the average book price and total number of books for each author, but only for authors who have written books costing more than $15
 
 select author, count(title), avg(price)
 from books
 where price > 15
 group by author;
 
-# Find the book with the highest price for each author, including authors who have not written any books.
+-- Find the book with the highest price for each author, including authors who have not written any books.
 
 select au.author_name, tablec.title, tablec.mp
 from
@@ -96,3 +96,66 @@ from books
 group by author) tableb
 on a.author = tableb.author and a.price = tableb.maxprice) tablec
 on au.author_name = tablec.author;
+
+-- Retrieve a list of book titles along with the names of their authors.
+
+select b.title, a.author_name
+from books as b
+inner join authors as a
+on b.author  = a.author_name;
+
+-- Display all authors and the titles of their books. Include authors who haven't written any books in the list.
+
+select a.author_name, b.title
+from authors as a
+left join
+books as b
+on a.author_name = b.author;
+
+-- Show all book titles along with their authors, including books that do not have a corresponding author in the authors table.
+
+select a.author_name, b.title
+from authors as a
+right join
+books as b
+on a.author_name = b.author;
+
+-- (Note: MySQL does not directly support FULL OUTER JOIN, but you can achieve this using a combination of LEFT and RIGHT JOINs). List all books and all authors, showing the pairing where they exist.
+
+SELECT a.author_name, b.title
+FROM authors a
+LEFT JOIN books b ON a.author_name = b.author
+
+UNION
+
+SELECT a.author_name, b.title
+FROM authors a
+RIGHT JOIN books b ON a.author_name = b.author;
+
+-- List all books along with their authors, but only include books in the 'Fantasy' genre and authors who are 'British'.
+
+select a.author_name, b.title
+from authors as a
+inner join
+books as b
+on a.author_name = b.author
+where a.nationality = 'British' and b.genre = 'Fantasy';
+
+-- Calculate the total number of books written by each author and display the author's name along with the count.
+
+select a.author_name, count(b.title)
+from authors as a
+left join
+books as b
+on a.author_name = b.author
+group by a.author_name;
+
+-- Select all authors who have written more than 3 books. Use a subquery within a join to determine the number of books each author has written.
+
+select a.author_name, count(b.title)
+from authors as a
+inner join 
+books as b
+on a.author_name = b.author
+group by a.author_name
+having count(b.title) > 2;
